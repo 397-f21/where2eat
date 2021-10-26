3eda90c6aea24b8738c968e496e1459b77043c30
petersenea <Petersenea12@gmail.com>
2021-10-25 19:06:09 -0500

    added a temporary api fix

--- src/App.js
+++ src/App.js
@@ -10,94 +10,102 @@ function App() {
   const API_KEY = "LL7utKGyZOsd7g1xdwfMyFEh80UCmesfPzQ5pfd6VeBexSbUbyRkBuGswVcMsqPFxNGYe7u63HIQWwpoKZuJFWM759wh6RwZZLLZYGEMbFyesMJrugHWrH4_YDdwYXYx";
   console.log(API_KEY);
 
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [errorMsg, setErrorMsg] = useState(null);
+  const [unavailable, setUnavailable] = useState(false);
   const [selected, setSelected] = useState(null);
   const [chosen, setChosen] = useState(null);
-  // const [data, setData] = useState(null);
+  const [apiData, setApiData] = useState(null);
   const url = 'https://api.yelp.com/v3/graphql';
+
+
   // append this before url, no spaces: https://cors-anywhere.herokuapp.com/
 
-  // useEffect(async () => {
-  //   setLoading(true);
+  useEffect(async () => {
 
-  //   const getGeoLocation = () => {
-  //     if (navigator.geolocation) {
-  //       navigator.geolocation.getCurrentPosition(pullData);
-  //       let unavailable = false;
-  //     } else {
-  //       let unavailable = true;
-  //     }
-  //   }
+    const getGeoLocation = () => {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(pullData);
+      } 
+      else {
+        setUnavailable(true);
+      }
+    }
 
-  //   const pullData = async (position) => {
-  //     const payload = `{
-  //       search(term:"restaurant",
-  //         latitude:${position.coords.latitude},
-  //         longitude:${position.coords.longitude},
-  //              radius:1609) {
-  //         total
-  //         business {
-  //           url
-  //           name
-  //           location {
-  //               address1
-  //           }
-  //           categories {
-  //               title
-  //           }
-  //           reviews {
-  //             text
-  //             rating
-  //             time_created
-  //             url
-  //           }
-  //         }
-  //       }
-  //     }`;
-  //     try {
+    const pullData = async (position) => {
+      const payload = `{
+        search(term:"restaurant",
+          latitude:${position.coords.latitude},
+          longitude:${position.coords.longitude},
+               radius:1609) {
+          total
+          business {
+            url
+            name
+            location {
+                address1
+            }
+            categories {
+                title
+            }
+            reviews {
+              text
+              rating
+              time_created
+              url
+            }
+          }
+        }
+      }`;
+      try {
 
-  //       const response = await axios.post("https://cors-anywhere.herokuapp.com/"+url, {
-  //         headers: {
-  //           'Authorization': `Bearer ${API_KEY}`,
-  //           'Content-Type': 'application/graphql',
-  //         },
-  //         withCredentials: true,
-  //         body: payload
-  //       });
+        const requestOptions = {
+          method: 'POST',
+          headers: {'Content-Type': 'application/graphql', 'Authorization': `Bearer ${API_KEY}`, 'Origin': '', 'Accept-Language': 'en_US'},
+          body: payload
+        }
 
-  //       const jsonData = response.data.json();
-  //       setLoading(false);
-  //       setData(jsonData);
+        fetch("https://cors-anywhere.herokuapp.com/"+url, requestOptions)
+        .then(response => response.json())
+        .then(data => {
+          setApiData(data);
+          setLoading(false);
+          console.log("Api data", data);
+        });
 
-  //       console.log(jsonData); // Remeber to remove me
+        // const response = await axios.post("https://cors-anywhere.herokuapp.com/"+url, {
+        //   headers: {
+        //     'Authorization': `Bearer ${API_KEY}`,
+        //     'Content-Type': 'application/graphql',
+        //   },
+        //   withCredentials: true,
+        //   body: payload
+        // });
 
-  //     } catch (error) {
-  //       setErrorMsg(error.message);
-  //     }
-  //   };
+        // const jsonData = response.data.json();
+        // setLoading(false);
+        // setApiData(jsonData);
 
-  //   getGeoLocation();
+        // console.log(jsonData); // Remeber to remove me
 
-  //   //unavailable ? setData(null) : pullData();
-  // }, [])
+      } catch (error) {
+        setErrorMsg(error.message);
+      }
+    };
 
-  // if (loading) return <h1>Loading the data...</h1>;
-  // if (errorMsg) return <h1>errorMsg</h1>;
-  // if (unavailable) return <h1>Please turn on your location services to use this app</h1>;
+    getGeoLocation();
 
-  // const filter = () => {
+  }, [])
 
-  // };
+  if (loading) return <h1>Loading the data...</h1>;
+  if (errorMsg) return <h1>errorMsg</h1>;
+  if (unavailable) return <h1>Please turn on your location services to use this app</h1>;
 
-  let businesses = data.data.search.business;
-  console.log(businesses);
+  let businesses = apiData.data.search.business;
   let categories = {};
   for (let i = 0; i < businesses.length; i++) {
     let category = businesses[i].categories[0].title;
-    console.log(category);
     if (category in categories) {
-      console.log(categories)
       categories[category].push(businesses[i]);
     } else {
       categories[category] = [businesses[i]];
@@ -112,7 +120,7 @@ function App() {
           <h1>{selected}</h1>
           <h2>{chosen.name}</h2>
           <p>{chosen.location.address1}</p>
-          <a href={chosen.url}>View on Yelp</a>
+          <a href={chosen.url} target="_blank">View on Yelp</a>
           <button onClick={() => {
             const num = Math.floor(Math.random() * categories[selected].length);
             setChosen(categories[selected][num]);
@@ -126,7 +134,7 @@ function App() {
         <div>
           <h1>Where2Eat</h1>
           {Object.keys(categories).map((category) => {
-            console.log(category);
+            // console.log(category);
             return (
               <button
                 key={category}
