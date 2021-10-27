e19ac0bff3a84d7a7294994a0281e08515316b4e
xyang3672 <xyang3672@bths.edu>
2021-10-26 20:53:29 -0500

    style restaurant page
    - style restaurant page
    - add link to google map

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import { useState, useEffect } from "react";
 import env from 'react-dotenv';
 import axios from "axios";
 import { data } from "./data";
+import "./restaurant.css";
+import MapIcon from './map-marked-alt-solid.svg';
+import YelpIcon from './yelp-svgrepo-com.svg';
 
 function App() {
 
@@ -30,7 +33,7 @@ function App() {
     }
 
     const pullData = async (position) => {
-      console.log(position.coords.longitude);
+      //console.log(position.coords.longitude);
       const payload = { "latitude": position.coords.latitude, "longitude": position.coords.longitude };
       try {
 
@@ -89,23 +92,27 @@ function App() {
     }
   }
 
+  
 
   return (
     <div className="App">
       {selected ?
-        <div>
-          <h1>{selected}</h1>
-          <h2>{chosen.name}</h2>
-          <p>{chosen.location.address1}</p>
-          <a href={chosen.url} target="_blank">View on Yelp</a>
-          <button onClick={() => {
+        <div className="restaurant">
+          <h1 className="category">{selected}</h1>
+          <h2 className="name">{chosen.name}</h2>
+          <hr />
+          <a className="address" href={"http://maps.google.com/?q=" + chosen.location.address1.replace(" ", "+")} target="_blank"><img src={MapIcon} width="15px" height="15px" style={{paddingRight: "10px"}}/>{chosen.location.address1}</a>
+          <a className="yelp" href={chosen.url} target="_blank"><img src={YelpIcon} width="15px" height="15px" style={{paddingRight: "10px"}}/>View on Yelp</a>
+          <div className="btn-group"> 
+          <button className="btn renew" onClick={() => {
             const num = Math.floor(Math.random() * categories[selected].length);
             setChosen(categories[selected][num]);
           }}>New {selected} Suggestion</button>
-          <button onClick={() => {
+          <button className="btn back" onClick={() => {
             setChosen(null);
             setSelected(null);
           }}>Back</button>
+          </div>
         </div>
         :
         <div>
