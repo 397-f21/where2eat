97a5881d430c0e4848acd010b7b7b9ed02398cfb
Yemi Kelani <aak.development@gmail.com>
2021-11-02 01:13:27 -0500

    Added Stars for restaurant Ratings - Yemi

--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -1,5 +1,6 @@
 import MapIcon from '../map-marked-alt-solid.svg';
 import YelpIcon from '../yelp-svgrepo-com.svg';
+import { BsStarFill, BsStarHalf, BsStar } from "react-icons/bs";
 import { useState } from "react";
 
 const Restaurant = ({ categories, selected, setSelected }) => {
@@ -7,6 +8,28 @@ const Restaurant = ({ categories, selected, setSelected }) => {
   const [chosen, setChosen] = useState(categories[selected][0]);
 
   var price_level = { null: 'Not available yet', '$': '$10 and under', '$$': 'Between $10-$25', '$$$': 'Between $25-$45', '$$$$': '$50 and up' };
+
+  // rating stars
+  let step = 0;
+  const stars = [];
+  let ratings = chosen.rating;
+  while (step < 5) {
+    if (ratings >= 1) {
+      // full star
+      stars.push(<BsStarFill />);
+      ratings--;
+      step++;
+    } else if (ratings < 1 && ratings > 0) {
+      // half star
+      stars.push(<BsStarHalf />);
+      ratings--;
+      step++;
+    } else {
+      // empty star
+      stars.push(<BsStar />);
+      step++;
+    }
+  }
 
   return (
 
@@ -17,7 +40,7 @@ const Restaurant = ({ categories, selected, setSelected }) => {
       <hr />
       <a className="address" href={"http://maps.google.com/?q=" + chosen.location.address1.replace(" ", "+")} target="_blank"><img src={MapIcon} width="15px" height="15px" style={{ paddingRight: "10px" }} />{chosen.location.address1}</a>
       <a className="yelp" href={chosen.url} target="_blank"><img src={YelpIcon} width="15px" height="15px" style={{ paddingRight: "10px" }} />View on Yelp</a>
-      <p>Rating: {chosen.rating}</p>
+      <p>Rating: {stars} ({chosen.rating})</p>
       <p>Price: {price_level[chosen.price]}</p>
 
       <div className="btn-group">
