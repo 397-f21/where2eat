eeff664d10f553545a8b4b6f858a355031596fff
Zheng Kaixin <574998054@qq.com>
2021-11-03 16:17:32 -0500

    add checkbox

--- src/components/Selectors.js
+++ src/components/Selectors.js
@@ -3,6 +3,38 @@ import "./price.css"
 
 const Selectors = ({ categories, setSelected, filteredCategories, setFilteredCategories, currDistance, setCurrDistance }) => {
     const [currPrice, setCurrPrice] = useState(0);
+    
+    const [checkedState, setCheckedState] = useState(
+        new Array(4).fill(false)
+    );
+    
+    const handleOnChange = (position) => {
+        const updatedCheckedState = checkedState.map((item, index) =>
+          index === position ? !item : item
+        );
+        setCheckedState(updatedCheckedState);
+        
+        const newCategories = {}
+        for (let key in categories) {
+            for (let restaurant of categories[key]) {
+                if(!updatedCheckedState[0] && ! updatedCheckedState[1] &&  !updatedCheckedState[2] && ! updatedCheckedState[3] && !  updatedCheckedState[4]){
+                    if (key in newCategories) {
+                        newCategories[key].push(restaurant);
+                    } else {
+                        newCategories[key] = [restaurant];
+                    }
+                }
+                else if ((restaurant.price == '$' && updatedCheckedState[0]) || (restaurant.price == '$$' && updatedCheckedState[1]) || (restaurant.price == '$$$' && updatedCheckedState[2]) || (restaurant.price == '$$$$' && updatedCheckedState[3]) || (restaurant.price == null)) {
+                    if (key in newCategories) {
+                        newCategories[key].push(restaurant);
+                    } else {
+                        newCategories[key] = [restaurant];
+                    }
+                }
+            }
+        };
+        setFilteredCategories(newCategories); 
+    };
 
     const priceButtonClick = (price) => {
         if (currPrice === price.length) {
@@ -34,6 +66,45 @@ const Selectors = ({ categories, setSelected, filteredCategories, setFilteredCat
 
     return (
         <div>
+            <div className="topping">
+            <input
+                    type="checkbox"
+                    id="topping"
+                    name="topping"
+                    value="Paneer"
+                    checked={checkedState[0]}
+                    onChange={() => handleOnChange(0)}
+            />
+            $
+            <input
+                    type="checkbox"
+                    id="topping"
+                    name="topping"
+                    value="Paneer"
+                    checked={checkedState[1]}
+                    onChange={() => handleOnChange(1)}
+            />
+            $$
+            <input
+                    type="checkbox"
+                    id="topping"
+                    name="topping"
+                    value="Paneer"
+                    checked={checkedState[2]}
+                    onChange={() => handleOnChange(2)}
+            />
+            $$$
+            <input
+                    type="checkbox"
+                    id="topping"
+                    name="topping"
+                    value="Paneer"
+                    checked={checkedState[3]}
+                    onChange={() => handleOnChange(3)}
+            />
+            $$$$
+            </div>
+
             <div id="distanceButtons">
                 <button className={currDistance === 0.5 ? "selected-distance-btn" : "distance-btn"} onClick={() => setCurrDistance(0.5)}>0.5 Miles</button>
                 <button className={currDistance === 1 ? "selected-distance-btn" : "distance-btn"} onClick={() => setCurrDistance(1)}>1 Mile</button>
