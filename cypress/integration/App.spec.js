87c2417604737f66c1bf7aeaf65d3ea4eb22e4db
Abby Heath <abbyheath@dhcp-10-105-202-153.wireless.northwestern.private>
2021-11-04 16:44:46 -0500

    abby yelp cypress test

--- cypress/integration/App.spec.js
+++ cypress/integration/App.spec.js
@@ -9,9 +9,18 @@ describe("Test App", () => {
     it('content test', () => {
         cy.get('[data-testid=banner]').contains('Find Your Meal!');
     })
-    it('interaction test', () => {
+    it.skip('interaction test', () => {
         cy.get('[data-testid=selection-button', { timeout: 10000 }).first().click();
         cy.get('[data-testid="back button"]');
+    })
+    it('yelp works', () => {
+        cy.get('[data-testid=selection-button', { timeout: 10000 }).first().click();
+        cy.get('[data-testid=yelpLink]')
+            .should('have.attr', 'href')
+            .then((href) => {
+                expect(href).to.have.string("https://www.yelp.com");
+            });
+
     })
 })
 
