c39d5fc88a41848e9c064eef7888632f7ebefec9
Zheng Kaixin <574998054@qq.com>
2021-11-04 22:29:48 -0500

    add FilterButton test

--- src/components/FilterButtons.js
+++ src/components/FilterButtons.js
@@ -28,7 +28,7 @@ const FilterButtons = ({ categories, setFilteredCategories, currDistance, setCur
     };
 
     return (
-        <div>
+        <div data-testid="FilterButtons">
             <div id="distanceButtons">
                 <button className={currDistance === 0.5 ? "selected-distance-btn" : "distance-btn"} onClick={() => setCurrDistance(0.5)}>&le; 0.5 Miles</button>
                 <button className={currDistance === 1 ? "selected-distance-btn" : "distance-btn"} onClick={() => setCurrDistance(1)}>&le; 1 Mile</button>
